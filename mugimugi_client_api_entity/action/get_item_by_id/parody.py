b76b928c2014efdb9781de55a9ba66705952dff5
MOLY Jean-Marc <jeanmarc.moly@gmail.com>
2021-06-13 10:56:05 +0200

    fix rename GetParodyById

--- mugimugi_client_api_entity/action/get_item_by_id/parody.py
+++ mugimugi_client_api_entity/action/get_item_by_id/parody.py
@@ -7,7 +7,7 @@ from ...root import ValidRoot
 
 
 @dataclass
-class Root(ValidRoot[Parody]):
+class GetParodyById(ValidRoot[Parody]):
     elements: list[Parody] = field(
         default_factory=list,
         metadata=dict(name=Parody.Meta.name, type=XmlType.ELEMENT, min_occurs=0),
